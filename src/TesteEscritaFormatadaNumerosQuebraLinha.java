c735c03c5363a98e8c2f2e71839fe23e08c94d66
Murilo Rocha <software@murilorocha.com.br>
2022-04-15 16:11:11 -0300

    TesteEscritaFormatadaNumerosQuebraLinha
    Teste de escrita de texto formatado com números e quebras de linhas usando o método printf em Java.
    
    Código feito utilizando o IDE Intellij IDEA.

---
+++ src/TesteEscritaFormatadaNumerosQuebraLinha.java
@@ -0,0 +1,6 @@
+public class TesteEscritaFormatadaNumerosQuebraLinha {
+
+    public static void main(String[] args) {
+        System.out.printf("Nota 1: %d\nNota 2: %d\nNota 3: %d", 100, 80, 95);
+    }
+}
